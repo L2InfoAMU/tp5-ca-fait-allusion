1f51dd67328a22f9ba54b29e97c29e3d5f79141d
MyceliumWay <thomas.pupunat@etu.univ-amu.fr>
2018-12-05 11:27:54 +0100

    implementation de Sparse : constructeurs createRepresentation setPixel getPixel

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -1,4 +1,65 @@
 package image;
 
-public class SparseRasterImage {
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static util.Matrices.*;
+import static util.Matrices.getRowCount;
+
+public class SparseRasterImage implements Map {
+
+    private int height;
+    private int width;
+    private Map<Point,Color> pixelsMap;
+
+    public SparseRasterImage(Color[][] colors){
+
+        requiresNonNull(colors);
+        requiresNonZeroDimensions(colors);
+        requiresRectangularMatrix(colors);
+
+        height = getColumnCount(colors);
+        width = getRowCount(colors);
+
+        createRepresentation();
+        setPixelsColor(colors);
+    }
+
+    public SparseRasterImage(Color color, int width, int height){
+        this.height = height;
+        this.width = width;
+
+        createRepresentation();
+        setPixelsColor(color);
+    }
+
+    public void createRepresentation(){
+        pixelsMap = new HashMap<>();
+
+
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+        Point myPoint = new Point(x, y);
+        pixelsMap.put(myPoint, color);
+    }
+
+    public Color getPixelColor(int x, int y){
+        return pixelsMap.get(new Point(x, y));
+    }
+
+    private void setPixelsColor(Color[][] pixels){
+        
+    }
+
+
+
+
+
+
+
+
+
 }
